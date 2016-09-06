023849314db3d39ee75f0fc65118a005060588e8
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2016-09-06 09:16:01 +0700

    first commit

---
+++ Assets/LogEnv.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LogEnv : MonoBehaviour {
+
+	public Text holder;
+	// Use this for initialization
+	void Start () {
+		#if DEVELOPMENT_BUILD || UNITY_EDITOR
+		Debug.Log("Development");
+		holder.text = "Development";
+		#else
+		Debug.Log("Production");
+		holder.text = "Production";
+		#endif
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
